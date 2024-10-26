83ffe4e62726145c777d6dbe9d37ea73f7ff49f0
Максим Євсеєв bc2024-4 <MAKSYM.YEVSEIEV@lnu.edu.ua>
2024-10-26 20:37:50 +0300

    implemented get method

--- index.js
+++ index.js
@@ -2,15 +2,41 @@ const { program } = require('commander');
 const superagent = require('superagent');
 const nodemon = require('nodemon');
 const http = require('http');
+const fs = require('fs').promises;
 program
 .option('-h, --host <server host>')
 .option('-p, --port <server port>')
 .option('-c, --cache <cache directory>');
 program.parse();
 const options = program.opts();
-    const server = http.createServer(function (req, res) {
-    res.write("currently running");
-    res.end();
+    const server = http.createServer(async (req, res) => {
+        if (req.method === 'GET') {
+            try {
+            const contents = await fs.readFile(options.cache + '/' + req.url.slice(1) + '.jpg');
+            res.writeHead(200, "OK", { "Content-Type": "image/jpeg" });
+            res.end(contents);
+            }
+            catch {
+            res.writeHead(404, "Not found", { "Content-Type": "text/plain" });
+            res.write("not found");
+            res.end();
+            }
+        }
+        else if (req.method === 'PUT') {
+        res.writeHead(201);
+        res.write("runnin");
+        res.end();
+        }
+        else if (req.method === 'DELETE') {
+        res.writeHead(200);
+        res.write("runnin");
+        res.end();
+        }
+        else {
+        res.writeHead(405, "Method not allowed", { "Content-Type": "text/plain" });
+        res.write("method not allowed");
+        res.end();
+        }
     });
     if (options.host === undefined || options.port === undefined || options.cache === undefined) {
     console.log("please specify host, port and cache if u want server to work");
